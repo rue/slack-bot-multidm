6d1410e7d272d472eded083eb619570504b1ac06
Eero Saynatkari <eero.saynatkari@reaktor.com>
2020-04-13 23:41:32 +0300

    Remove test message handling

--- app.ts
+++ app.ts
@@ -17,51 +17,6 @@ app.error(async (error) => {
   console.error({ type: "error", event: "fallbackErrorHandler", error });
 });
 
-app.message("hello", async ({ message, say, logger }) => {
-  logger.info({ type: "message", event: "hello", message });
-
-  /* eslint-disable @typescript-eslint/camelcase */
-  const section: SectionBlock = {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: `Hey there <@${message.user}>!`,
-    },
-    accessory: {
-      type: "button",
-      text: {
-        type: "plain_text",
-        text: "Click Me",
-      },
-      action_id: "hello_button_click",
-    },
-  };
-
-  try {
-    await say({
-      blocks: [section],
-      text:
-        "This say() contains a section block; this text should not be displayed",
-    });
-  } catch (error) {
-    logger.error({ type: "error", event: "hello", error });
-  }
-});
-
-// Actions
-
-app.action("hello_button_click", async ({ body, ack, say, logger }) => {
-  logger.info({ type: "action", event: "hello_button_click", body });
-
-  try {
-    await ack();
-
-    await say(`<@${body.user.id}> clicked the button, the dummy!`);
-  } catch (error) {
-    logger.error({ type: "error", event: "hello click", error });
-  }
-});
-
 // Commands
 
 app.command("/multidm", async ({ command, ack, say, logger }) => {
